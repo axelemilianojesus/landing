e46573a0e56d747a3b2f20a205e87a81ea599ac4
Axel Emiliano Rodriguez <azel@MacBook-Pro-de-Axel.local>
2025-05-03 10:19:45 +0900

    Fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const { withFaust, getWpHostname } = require('@faustwp/core');
-const { createSecureHeaders } = require('next-secure-headers');
 
-/**
- * @type {import('next').NextConfig}
- **/
+/** @type {import('next').NextConfig} */
 module.exports = withFaust({
   reactStrictMode: true,
   sassOptions: {
@@ -16,9 +13,5 @@ module.exports = withFaust({
     locales: ['en'],
     defaultLocale: 'en',
   },
-  async headers() {
-    return [{ source: '/:path*', headers: createSecureHeaders({
-      xssProtection: false
-    }) }];
-  },
+  // Eliminado el bloque headers() que usaba next-secure-headers
 });
